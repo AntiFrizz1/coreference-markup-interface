77bd4978734fedfef000602812a6360371cf8f44
AntiFrizz1 <gluhov1998@yandex.ru>
2019-02-25 00:59:07 +0300

    Add simple server

--- src/server/ServerImpl.java
+++ src/server/ServerImpl.java
@@ -1,9 +1,17 @@
 package server;
 
+import document.Document;
+import document.DocumentImpl;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * This class describes interaction protocol of Server
@@ -28,10 +36,24 @@ public class ServerImpl implements Server {
     private List<List<String>> texts;
 
     /**
+     * Pool of clients.
+     */
+    private List<Socket> clients;
+
+    /**
      * Pool of users.
      */
     private List<Socket> users;
 
+    /**
+     * Pool of users.
+     */
+    private List<Socket> onlineUsers;
+
+    /**
+     * Pool of users.
+     */
+    private List<Socket> offlineUsers;
 
     /**
      * Set of judges id.
@@ -41,5 +63,343 @@ public class ServerImpl implements Server {
     /**
      * Pool of judges.
      */
-    private List<Socket> judges;
+    private List<JudgeInfo> judges;
+
+    /**
+     * Set of selected texts
+     */
+    private Set<Integer> selectedTexts;
+
+    /**
+     * List of workers
+     */
+    private List<Thread> workers;
+
+    /**
+     * List of conflicts
+     */
+    private List<ConflictInfo> conflicts;
+
+
+    public ServerImpl() {
+        try {
+            socket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        users = new CopyOnWriteArrayList<>();
+        judges = new CopyOnWriteArrayList<>();
+        clients = new CopyOnWriteArrayList<>();
+        offlineUsers = new CopyOnWriteArrayList<>();
+        onlineUsers = new CopyOnWriteArrayList<>();
+        workers = new CopyOnWriteArrayList<>();
+
+        judgesId = new ConcurrentSkipListSet<>();
+        selectedTexts = new ConcurrentSkipListSet<>();
+    }
+
+    /**
+     * Start server
+     * @throws InterruptedException
+     */
+    public void run() throws InterruptedException {
+        Thread listenerThread = new Thread(listener);
+        Thread schedulerThread = new Thread(scheduler);
+        Thread userSchedulerThread = new Thread(userScheduler);
+        Thread onlineUsersSchedulerThread = new Thread(onlineUsersScheduler);
+        Thread offlineUsersSchedulerThread = new Thread(offlineUsersScheduler);
+
+        listenerThread.start();
+        schedulerThread.start();
+        userSchedulerThread.start();
+        onlineUsersSchedulerThread.start();
+        offlineUsersSchedulerThread.start();
+
+        listenerThread.join();
+        schedulerThread.join();
+        userSchedulerThread.join();
+        onlineUsersSchedulerThread.join();
+        offlineUsersSchedulerThread.join();
+    }
+
+    /**
+     * Connect clients to server
+     */
+    private Runnable listener = () -> {
+        while (true) {
+            try {
+                Socket client = socket.accept();
+                System.out.println("listener :=: connected: " + client.toString());
+                clients.add(client);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    /**
+     * Split clients on clients or judges
+     */
+    private Runnable scheduler = () -> {
+        try {
+            while (true) {
+                if (!clients.isEmpty()) {
+                    Socket client = clients.remove(0);
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())));
+
+                        String request = reader.readLine();
+                        System.out.println("scheduler :=: request from " + client.toString() + " = " + request);
+
+                        int id = Integer.parseInt(request);
+                        if (judgesId.contains(id)) {
+                            judges.add(new JudgeInfo(client));
+                        } else {
+                            users.add(client);
+                        }
+
+                        writer.println("OK");
+                        writer.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Scheduler interrupted: " + e.getMessage());
+        }
+    };
+
+    /**
+     * Split users on online or offline users
+     */
+    private Runnable userScheduler = () -> {
+        try {
+            while (true) {
+                if (!users.isEmpty()) {
+                    Socket client = users.remove(0);
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())));
+
+                        String request = reader.readLine();
+                        System.out.println("userScheduler :=: request from " + client.toString() + " = " + request);
+
+                        int id = Integer.parseInt(request);
+                        if (id == 0) {
+                            onlineUsers.add(client);
+                        } else {
+                            offlineUsers.add(client);
+                        }
+
+                        writer.println("OK");
+                        writer.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    /**
+     * Give task for online users
+     */
+    private Runnable onlineUsersScheduler = () -> {
+        int textNumber = 0;
+        try {
+            while (true) {
+                if (onlineUsers.size() >= 2) {
+                    Socket client1 = onlineUsers.remove(0);
+                    Socket client2 = onlineUsers.remove(0);
+                    Thread worker = new Thread(() -> {
+                        try {
+                            DocumentImpl document1;
+                            DocumentImpl document2;
+
+                            BufferedReader reader1 = new BufferedReader(new InputStreamReader(client1.getInputStream()));
+                            BufferedReader reader2 = new BufferedReader(new InputStreamReader(client2.getInputStream()));
+
+                            PrintWriter writer1 = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client1.getOutputStream())));
+                            PrintWriter writer2 = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client2.getOutputStream())));
+
+                            writer1.println(/*document.pack()*/);
+                            writer2.println(/*document.pack()*/);
+
+                            writer1.flush();
+                            writer2.flush();
+
+                            Thread thread1 = new Thread(() -> {
+                                while (true) {
+                                    try {
+                                        String request = reader1.readLine();
+                                        //process data
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+
+                            Thread thread2 = new Thread(() -> {
+                                while (true) {
+                                    try {
+                                        String request = reader2.readLine();
+                                        //process data
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                            thread1.start();
+                            thread2.start();
+                            thread1.join();
+                            thread2.join();
+                        } catch (IOException | InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    worker.start();
+                    workers.add(worker);
+                } else {
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    /**
+     * Give task for offline users
+     */
+    private Runnable offlineUsersScheduler = () -> {
+        try {
+            while (true) {
+                if (!offlineUsers.isEmpty()) {
+                    Socket client = offlineUsers.remove(0);
+                    System.out.println("offlineUsersScheduler :=: " + client.toString());
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())));
+
+                        writer.println("doc");
+                        writer.flush();
+                        Thread worker = new Thread(() -> {
+                            try {
+                                String doc = reader.readLine();
+                                //save data
+                                System.out.println("offlineUsersScheduler :=: " + client.toString() + " doc = " + doc);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+                        worker.run();
+                        workers.add(worker);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    /*Runnable judgeScheduler = () -> {
+        try {
+            while (true) {
+
+            }
+        }
+    };*/
+
+    class JudgeInfo {
+        Socket socket;
+        AtomicReference<ConflictInfo> task;
+        Thread worker;
+
+        JudgeInfo(Socket socket) {
+            this.socket = socket;
+            task = new AtomicReference<>(null);
+            worker = new Thread(judgeWorker);
+            worker.start();
+        }
+
+        boolean setTask(ConflictInfo task) {
+            return (this.task.compareAndSet(null, task));
+        }
+
+        Runnable judgeWorker = () -> {
+            try {
+                while (true) {
+                    if (task.get() != null) {
+                        task.get().apply();
+                        //process
+                    } else {
+                        Thread.sleep(1000);
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+    }
+
+    /**
+     * Consist data about conflict
+     */
+    class ConflictInfo {
+        Document document;
+        AtomicInteger status;
+        Thread counter;
+
+        ConflictInfo(Document document) {
+            this.document = document;
+            this.status = new AtomicInteger(0);
+        }
+
+        boolean setDocument(Document document) {
+            if (status.compareAndSet(1, 2)) {
+                this.document = document;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        boolean again() {
+            int localStatus = status.get();
+            return localStatus != 0 && status.compareAndSet(localStatus, 0);
+        }
+
+        boolean apply() {
+            if (status.compareAndSet(0, 1)) {
+                counter = new Thread(() -> {
+                    while (status.get() != 2) {
+                        try {
+                            Thread.sleep(100000);
+                            again();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
 }
