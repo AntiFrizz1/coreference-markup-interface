fdcded64a825f5727863d2e96ec23a7929441297
AntiFrizz1 <gluhov1998@yandex.ru>
2019-02-28 15:47:12 +0300

    Replace Action for Letter

--- src/document/ConflictInfo.java
+++ src/document/ConflictInfo.java
@@ -1,6 +1,7 @@
 package document;
 
 import chain.Action;
+import chain.Letter;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,15 +13,15 @@ public class ConflictInfo {
     public int teamOneId;
     public int teamTwoId;
 
-    public Action action1;
-    public Action action2;
+    public Letter letter1;
+    public Letter letter2;
 
     public AtomicInteger status;
     public Thread counter;
 
     public ConflictInfo(ConflictData data) {
-        this.action1 = data.action1;
-        this.action2 = data.action2;
+        this.letter1 = data.letter1;
+        this.letter2 = data.letter2;
         this.teamOneId = data.teamOneId;
         this.teamTwoId = data.teamTwoId;
         this.textId = data.textId;
