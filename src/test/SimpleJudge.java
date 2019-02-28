92dbd8796ec9dd4629ff5f22e23e91cccf701385
AntiFrizz1 <gluhov1998@yandex.ru>
2019-02-28 19:33:49 +0300

    Add test

---
+++ src/test/SimpleJudge.java
@@ -0,0 +1,17 @@
+package test;
+
+import client.Conflict;
+import client.Judge;
+
+public class SimpleJudge {
+    public static void main(String[] args) throws InterruptedException {
+        Judge judge = new Judge(228, 3333, "localhost");
+        judge.joinOnline();
+        Conflict conflict = judge.getInfo();
+        Thread.sleep(5000);
+        judge.sendDecision(3);
+        conflict = judge.getInfo();
+        Thread.sleep(5000);
+        judge.sendDecision(3);
+    }
+}
